def23d0b7da866232d5bbf00ff1967826284eb99
George Talusan <george.talusan@gmail.com>
2021-09-25 13:55:52 -0400

    enable cli and fix bluez dep

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 autoreconf --install
-./configure --host=arm-linux --with-sysroot=/chroot --prefix= --with-alsaplugindir=/lib/alsa-lib --disable-debug --enable-mpg123
+./configure --host=arm-linux --with-sysroot=/chroot --prefix= --with-alsaplugindir=/lib/alsa-lib --disable-debug --enable-mpg123 --enable-cli
